ee2830de9422e4953bbbb76deedbc5c371ad82a5
Richard Connon <richard@connon.me.uk>
2017-07-15 10:43:44 +0100

    Adding testing for external runtime. Just trying to make a request for now.

--- hunts/runtimes/tests.py
+++ hunts/runtimes/tests.py
@@ -1,26 +1,20 @@
 # vim: set fileencoding=utf-8 :
 from unittest import TestCase
 
+from .external import ExternalRuntime
 from .regex import RegexRuntime
 from .static import StaticRuntime
 
+import requests
 
-class StaticRuntimeTestCase(TestCase):
-    def test_evaluate(self):
-        static_runtime = StaticRuntime()
-        static_script = '''Hello  World!'''
-        result = static_runtime.evaluate(static_script, None, None, None, None)
-        self.assertEqual(result, static_script)
 
-    def test_validate_guess(self):
-        static_runtime = StaticRuntime()
-        static_script = '''answer'''
-        guess1 = "answer"
-        result = static_runtime.validate_guess(static_script, guess1, None, None)
-        self.assertTrue(result)
-        guess2 = "incorrect answer"
-        result = static_runtime.validate_guess(static_script, guess2, None, None)
-        self.assertFalse(result)
+class ExternalRuntimeTestCase(TestCase):
+    def test_connection(self):
+        external_runtime = ExternalRuntime()
+        external_uri = r'http://example:8080/content'
+        r = requests.post(external_uri)
+        r.raise_for_status()
+
 
 
 class RegexRuntimeTestCase(TestCase):
@@ -45,3 +39,21 @@ class RegexRuntimeTestCase(TestCase):
         regex_script = r'[]'
         with self.assertRaises(SyntaxError):
             regex_runtime.validate_guess(regex_script, "", None, None)
+
+
+class StaticRuntimeTestCase(TestCase):
+    def test_evaluate(self):
+        static_runtime = StaticRuntime()
+        static_script = '''Hello  World!'''
+        result = static_runtime.evaluate(static_script, None, None, None, None)
+        self.assertEqual(result, static_script)
+
+    def test_validate_guess(self):
+        static_runtime = StaticRuntime()
+        static_script = '''answer'''
+        guess1 = "answer"
+        result = static_runtime.validate_guess(static_script, guess1, None, None)
+        self.assertTrue(result)
+        guess2 = "incorrect answer"
+        result = static_runtime.validate_guess(static_script, guess2, None, None)
+        self.assertFalse(result)
