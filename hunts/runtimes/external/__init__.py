001afe9ca3c707dfced9573f0c18b9c6e49bdc36
Richard Connon <richard@connon.me.uk>
2017-07-16 19:31:59 +0100

    Add external validate_guess method

--- hunts/runtimes/external/__init__.py
+++ hunts/runtimes/external/__init__.py
@@ -1,7 +1,6 @@
 # vim: set fileencoding=utf-8 :
 from .. import AbstractRuntime
 
-import json
 import requests
 
 
@@ -23,15 +22,27 @@ class ExternalRuntime(AbstractRuntime):
 
         response = r.json()
         if 'team_puzzle_data' in response:
-            team_puzzle_data = response['team_puzzle_data']
+            team_puzzle_data.data = response['team_puzzle_data']
         if 'user_puzzle_data' in response:
-            user_puzzle_data = response['user_puzzle_data']
+            user_puzzle_data.data = response['user_puzzle_data']
         if 'team_data' in response:
-            team_data = response['team_data']
+            team_data.data = response['team_data']
         if 'user_data' in response:
-            user_data = response['user_data']
+            user_data.data = response['user_data']
 
         return response['content']
 
     def validate_guess(self, endpoint, guess, team_puzzle_data, team_data):
-        return False
+        team_id = team_data.team.pk
+        payload = {
+            'guess': guess,
+            'team_id': team_id,
+            'team_puzzle_data': team_puzzle_data.data,
+            'team_data': team_data.data,
+        }
+
+        r = requests.post(endpoint, json=payload)
+        r.raise_for_status()
+
+        response = r.json()
+        return response['correct']
