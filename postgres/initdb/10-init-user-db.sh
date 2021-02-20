d3bcac17bfc93345b7f71aeedf5c449ee278588f
Richard Connon <richard@connon.me.uk>
2021-02-20 22:39:29 +0000

    Improved DB migration instructions
    Having actually migrated the DB for two prod sites I've come up with a
    much better set of instructions for this. Sadly I'll never get to follow
    them but maybe it will help someone else.
    This also adds an extra user for the postgres exporter to use with
    privilege to read all the things it needs delegated to it from the
    superuser via stored procedure magic.

--- postgres/initdb/10-init-user-db.sh
+++ postgres/initdb/10-init-user-db.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 set -euo pipefail
 
-[ -z "${H2_DATABASE_PASSWORD:-}" ] || psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" --dbname "${POSTGRES_DB}" <<-EOSQL
+[ -n "${H2_DATABASE_PASSWORD:-}" ] || return 0
+
+psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" --dbname "${POSTGRES_DB}" <<-EOSQL
     CREATE USER hunter2;
-    CREATE DATABASE hunter2 OWNER hunter2;
     ALTER USER hunter2 WITH PASSWORD '${H2_DATABASE_PASSWORD}';
+    CREATE DATABASE hunter2 OWNER hunter2;
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" --dbname hunter2 <<-EOSQL
+    GRANT ALL ON SCHEMA public TO hunter2;
+    REVOKE ALL ON SCHEMA public FROM postgres;
+    REVOKE ALL ON SCHEMA public FROM PUBLIC;
 EOSQL
